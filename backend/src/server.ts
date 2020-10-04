0789f84a90eeff060b6d6193dadb9ef92635637b
Heltonphg <wylammi.helton@gmail.com>
2020-10-04 12:12:15 -0300

    configurando o banco de dados

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,12 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log("Listagem de usuários");
-  response.send("Hello");
-});
+app.use(express.json())
+app.use(routes);
 
 app.listen(3333, () =>{
   console.log("Rodando na porta 3333");
 });
+
