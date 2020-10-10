eaea370b8990c3bbbc37cbd13410fce6a4ac0cde
Heltonphg <wylammi.helton@gmail.com>
2020-10-10 16:38:52 -0300

    items

--- backend/src/controllers/ItemsController.ts
+++ backend/src/controllers/ItemsController.ts
@@ -4,15 +4,16 @@ import knex from '../database/connection';
 class ItemsController {
   async index(request: Request, response: Response) {
     const items = await knex('items').select('*');
-  
+    const IP = '10.0.0.105'
+
     const serializedItems = items.map(item => {
       return {
         id: item.id,
         name: item.title,
-        image_url: `http://localhost:3333/uploads/${item.image}`,
+        image_url: `http://${IP}:3333/uploads/${item.image}`,
       }
     });
-  
+
     response.json(serializedItems);
   }
 }
